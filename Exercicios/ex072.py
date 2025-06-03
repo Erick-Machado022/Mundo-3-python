19015fe2d674d76f2216669a39aa4e4696bad7ac
Erick <erickmachado050@gmail.com>
2025-06-03 19:53:13 -0300

    Add continuar no programa

--- Exercicios/ex072.py
+++ Exercicios/ex072.py
@@ -5,9 +5,15 @@ e mostrá-lo por extenso.'''
 
 contagem = ('zero','um','dois','tres','quatro','cinco','seis','sete','oito','nove','dez',
             'onze','doze','treze','quatorze','quinze','dezesseis','dezessete','dezoito','dezenove','vinte',)
-
 while True:
-    n = int(input('Digite um número entre 0 e 20: '))
-    if 0 <= n <=20:
+    while True:
+        n = int(input('Digite um número entre 0 e 20: '))
+        if 0 <= n <=20:
+            break
+        print('Número inválido, tente novamente:')
+    print(f'Você digitou o número {contagem[n]}')
+
+    pergunta = input('você quer continuar? [S/N]').strip().upper()[0]
+    if pergunta == 'N':
         break
-print(f'Você digitou o número {contagem[n]}')
+print('Programa finalizado.')
