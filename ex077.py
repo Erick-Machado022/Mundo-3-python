d8129ec100c8f37a99b788f97527de0246ef2e63
Erick <erickmachado050@gmail.com>
2025-06-10 12:30:28 -0300

    delet exercicios fora da pasta

--- ex077.py
+++
@@ -1,9 +0,0 @@
-palavra = ('casa', 'hotel', 'programar', 'python', 'computador', 'estudar', 'futuro', 'mercado', 'loja', 'papelaria')
-print('=-='*15)
-print(f'{"Contador de vogais":^45}')
-print('=-='*15)
-for p in palavra:
-    print(f'\nA palavra {p} tem as vogais:', end='')
-    for letra in p:
-        if letra in 'aeiou':
-            print(f'{letra}',end=' ')
