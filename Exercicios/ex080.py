10874ce5cdf83d627dcd2e7b62135c3ad8b86d9d
Erick <erickmachado050@gmail.com>
2025-06-16 13:58:39 -0300

    Finalização do exercicio

--- Exercicios/ex080.py
+++ Exercicios/ex080.py
@@ -1,5 +1,15 @@
 lista = []
-for c in range(0,5):
-    num = int(input('Digite um valor: '))
-    lista.append(num)
-print(lista)
+for c in range(0, 5):
+    num = int(input(f'Digite o {c+1}º valor: '))
+    if c == 0 or num > lista[-1]:
+        lista.append(num)
+    else:
+        pos = 0
+        while pos < len(lista):
+            if num <= lista[pos]:
+                lista.insert(pos, num)
+                break
+            pos += 1
+
+print('-' * 40)
+print(f'A lista ordenada é: {lista}')
