9a018d5ca864cdb85f83bd4d02002dada8d3e6c4
Erick <erickmachado050@gmail.com>
2025-07-29 19:24:53 -0300

    Aprimorando o exercicio 93

---
+++ Exercicios/ex095.py
@@ -0,0 +1,44 @@
+time = []
+jogador = {}
+partidas = []
+while True:
+    jogador.clear()
+    jogador['nome'] = input('Nome do Jogador: ')
+    jogos = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+    partidas.clear()
+    for p in range(jogos):
+        partidas.append(int(input(f'   Quantos gols na partida {p+1}? ')))
+    jogador['gols'] = partidas[:]
+    jogador['total'] = sum(partidas)
+    time.append(jogador.copy())
+    while True:
+        opc = input('Quer continuar? [S/N]').upper().strip()[0]
+        if opc in 'SN':
+            break
+        print('ERRO" Responda apenas S ou N.')
+    if opc == 'N':
+        break
+print('-='*30)
+print(f'{"cod":<4}', end='')
+for i in jogador.keys():
+    print(f'{i:<15}', end='')
+print()
+print('-'*60)
+for k, v in enumerate(time):
+    print(f'{k:<4}', end='')
+    for dado in v.values():
+        print(f'{str(dado):<15}', end='')
+    print()
+print('-='*30)
+while True:
+    busca = int(input('Mostrar dados de qual jogador?  [999 para parar] '))
+    if busca == 999:
+        break
+    if busca >= len(time):
+        print(f'Erro não existe jogador com o código da {busca}')
+    else:
+        print(f'-- LEVANTAMENTO DO JOGADOR {time[busca]["nome"]}:')
+        for i,g in enumerate(time[busca]['gols']):
+            print(f'   No jogo {i+1} fez {g} gols')
+    print('-'*40)
+print('<<< VOLTE SEMPRE >>>')
