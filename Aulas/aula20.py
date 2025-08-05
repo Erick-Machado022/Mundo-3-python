d62b6dd53742d3a872a49a95903e036a2fff1ef3
Erick <erickmachado050@gmail.com>
2025-08-05 18:12:37 -0300

    Desempacotando funções

--- Aulas/aula20.py
+++ Aulas/aula20.py
@@ -1,10 +1,9 @@
-def dobra(lista): # Função que dobra os valores de uma lista
-    pos = 0 #Inicia a lista na posição 0
-    while pos < len(lista): #Enquanto a posição for menor que o tamanho da lista
-        lista[pos] *= 2 #Dobra o valor do elemento na posição atual
-        pos += 1 #avança para o proximo número da lista
+def soma(* numeros):
+    s = 0
+    for valor in numeros:
+        s += valor
+    print(f'A soma dos valores {numeros} resultam em {s}')
 
 
-numeros = [2, 5 , 4 ,8 , 4, 2, 6]
-dobra(numeros)
-print(numeros)
+soma(8, 3)
+soma(7, 5, 1)
