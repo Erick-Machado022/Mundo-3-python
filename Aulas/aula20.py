dd37b58d1f9cb7dc1ea518fc93a67aba1c10a902
Erick <erickmachado050@gmail.com>
2025-08-05 18:05:22 -0300

    Função para dobrar os valores em uma lista

--- Aulas/aula20.py
+++ Aulas/aula20.py
@@ -1,7 +1,10 @@
-def contador(*num):
-    tam = len(num)
-    print(f'Recebi os valores {num} e ao total são {tam} números')
+def dobra(lista): # Função que dobra os valores de uma lista
+    pos = 0 #Inicia a lista na posição 0
+    while pos < len(lista): #Enquanto a posição for menor que o tamanho da lista
+        lista[pos] *= 2 #Dobra o valor do elemento na posição atual
+        pos += 1 #avança para o proximo número da lista
 
-contador(2, 1 ,7)
-contador(8, 0 )
-contador(4, 4, 6, 7)
+
+numeros = [2, 5 , 4 ,8 , 4, 2, 6]
+dobra(numeros)
+print(numeros)
