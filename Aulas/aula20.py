a034dd3de7aa3d2865e7b8f7bfd774633626db59
Erick <erickmachado050@gmail.com>
2025-08-05 17:24:37 -0300

    Primeiros comandos def em python

---
+++ Aulas/aula20.py
@@ -0,0 +1,11 @@
+def mostrarLinha():  # Função que imprime uma linha com 40 traços
+    print('-'*40)
+
+
+mostrarLinha() # Exibe uma linha separadora
+print(f'{"SISTEMA DE ALUNOS":^40}')
+mostrarLinha()
+print(f'{"CADASTRO DE ALUNOS":^40}')
+mostrarLinha()
+print(f'{"ERRO SISTEMA":^40}')
+mostrarLinha()
