3973010b59bc75caf85f577a02a67dd43f5fe602
Erick <erickmachado050@gmail.com>
2025-09-23 14:18:39 -0300

    ex108 formatando moedas em python

---
+++ Exercicios/ex108/ex108.py
@@ -0,0 +1,7 @@
+import moeda
+
+preço = float(input('Digite um preço: R$ '))
+
+print(f'A metade de {moeda.moeda(preço)} é {moeda.moeda(moeda.metade(preço))}')
+print(f'O dobro de {moeda.moeda(preço)} é {moeda.dobro(preço)}')
+print(f'Aumentando 10%, temos {moeda.moeda(moeda.aumentar(preço, 10))}')
