b20945d4ea14192ff10949b05ba08ec75e3cd7fe
Erick <erickmachado050@gmail.com>
2025-09-29 14:24:30 -0300

    Verificando se o site está online

---
+++ Exercicios/ex114.py
@@ -0,0 +1,10 @@
+import urllib
+import urllib.request
+
+while True:
+    try:
+        site = urllib.request.urlopen('https://dentistaalexandremonteiro.com.br/') #Tenta abrir a URL informada (acessar o site)
+    except:
+        print('O site não está acessível')
+    else:
+        print('O site está ok')
